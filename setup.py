61ad12a6bf9f5e218d519d2ab89a635c89eab012
BrianPetkovsek <16124109+BrianPetkovsek@users.noreply.github.com>
2022-05-13 08:45:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup
 
+import os
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
 setup(
     name='Env_Settings',
     version='0.0.1',
@@ -9,5 +17,6 @@ setup(
     author_email='contactnorthedgecomputers@gmail.com',
     license='unlicense',
     packages=['Env_Settings'],
+	install_requires=install_requires,
     zip_safe=False
 )
